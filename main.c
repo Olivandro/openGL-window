37d4b49e765253bda3bb60bd579ac32bd22ae5cd
Olivandro <aball615@icloud.com>
2026-05-08 17:07:40 +0100

    Building correctly now on arch linux.

--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <string.h>
-#include <assert.h>
 #include <stdbool.h>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
@@ -56,54 +55,54 @@ struct ShaderSource
 
 struct ShaderSource parseShader(const char filepath[])
 {
-    // instantiate a temp version of our struct
-    struct ShaderSource tmp;
+	// instantiate a temp version of our struct
+	struct ShaderSource tmp;
 
-    // Open our target file for parsing. This is the shader source
-    FILE *fp = fopen(filepath, "r");
+	// Open our target file for parsing. This is the shader source
+	FILE *fp = fopen(filepath, "r");
 
-    // Establish a buffer array for the file stream 
-    const char buffer[512];
+	// Establish a buffer array for the file stream 
+	char buffer[512];
 
-    // unsigned char ptr array that has the same size array as the buffer and 
-    // struct variables. This is probably not needed... Checking in on 
-    // that.
-    unsigned char (*src)[512];
+	// unsigned char ptr array that has the same size array as the buffer and 
+	// struct variables. This is probably not needed... Checking in on 
+	// that.
+	char (*src)[512];
 
-    // Determiners... These values determine when parsing if source code pertains to
-    // a vertex or a fragment shader.
-    char divider[] = "#shader";
-    char version[] = "#version";
-    char v[] = "vertex";
-    char f[] = "fragment";
+	// Determiners... These values determine when parsing if source code pertains to
+	// a vertex or a fragment shader.
+	char divider[] = "#shader";
+	char version[] = "#version";
+	char v[] = "vertex";
+	char f[] = "fragment";
 
-    while(fgets(buffer, 512, fp))
-    {
-        if (strstr(buffer, divider))
-        {
-            if (strstr(buffer, v))
-            {
-                src = &tmp.VertexShader;
-            }
-            else if (strstr(buffer, f))
-            {
-                src = &tmp.FragmentShader;
-            }
-        }
-        else if (strstr(buffer, version))
-        {
-            strcpy(src, buffer);
-        }
-        else
-        {
-            strcat(src, buffer);
-        }
-    }
-    // Lets close that file as we don't need it.
-    fclose(fp);
+	while(fgets(buffer, 512, fp))
+	{
+		if (strstr(buffer, divider))
+		{
+			if (strstr(buffer, v))
+			{
+				src = &tmp.VertexShader;
+			}
+			else if (strstr(buffer, f))
+			{
+				src = &tmp.FragmentShader;
+			}
+		}
+		else if (strstr(buffer, version))
+		{
+			strcpy(*src, buffer);
+		}
+		else
+		{
+			strcat(*src, buffer);
+		}
+	}
+	// Lets close that file as we don't need it.
+	fclose(fp);
 
-    // Return our parsed shader sources.
-    return tmp;
+	// Return our parsed shader sources.
+	return tmp;
 }
 
 // Shader compiler. Embedded function, used within createShader
@@ -155,10 +154,10 @@ unsigned int createShader(const char vertexShader[], const char fragmentShader[]
 // vertexBuffer function generates a buffer, binds it and assigns it data
 // On successful pass return 0, otherwise GLCall will return -1 with corresponding
 // error code.
-int vertexBuffer(unsigned int buffer, const void* data, unsigned int size)
+int vertexBuffer(unsigned int *buffer, const void* data, unsigned int size)
 {
-    GLCall(glGenBuffers(1, &buffer));
-    GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));
+    GLCall(glGenBuffers(1, buffer));
+    GLCall(glBindBuffer(GL_ARRAY_BUFFER, *buffer));
     GLCall(glBufferData(GL_ARRAY_BUFFER, size, data, GL_STATIC_DRAW));
     return 0;
 } 
@@ -179,10 +178,10 @@ int vertexBufferUnbind()
 // and bind the corresponding data to the ib object. We do not gen a buffer
 // for the ibo, because this causes a program crash with no error returned.
 // This function must be called after glGenBuffers(GLsizei n, GLuint* buffers)
-int indexBuffer(unsigned int ibo, const unsigned int* data, unsigned int count)
+int indexBuffer(unsigned int *ibo, const unsigned int* data, unsigned int count)
 {
-    GLCall(glGenBuffers(1, &ibo));
-    GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
+    GLCall(glGenBuffers(1, ibo));
+    GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, *ibo));
     GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, count, data, GL_STATIC_DRAW));
     return 0;
 }
@@ -268,7 +267,7 @@ int main(void)
 
     // Binding for vertex Buffer
     unsigned int buffer;
-    vertexBuffer(buffer, positions, 4 * 2 * sizeof(float));
+    vertexBuffer(&buffer, positions, 4 * 2 * sizeof(float));
     
     GLCall(glEnableVertexAttribArray(0));
     GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));
@@ -276,7 +275,7 @@ int main(void)
     // Binding for index buffer
     unsigned int ibo;
     assert(sizeof(unsigned int) == sizeof(GLuint));
-    indexBuffer(ibo, indices, 6 * sizeof(unsigned int));
+    indexBuffer(&ibo, indices, 6 * sizeof(unsigned int));
 
     char filepath[] = "shaders/basic.shader";
     struct ShaderSource Source = parseShader(filepath);
@@ -315,7 +314,7 @@ int main(void)
         indexBufferBind(ibo);
 
         // Draw pull or function..
-        GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, &indices));
+        GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, NULL));
         
         // Colour animation.
         if (r > 1.0f)
